31a109398e58eb0348382f4186975edadd88a539
Filip <super.dewolaj@gmail.com>
2016-08-09 19:15:26 +0200

    Dodanie rozwijanej listy trybów

--- src/com/example/Window.java
+++ src/com/example/Window.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
@@ -12,11 +13,27 @@ public class Window extends JFrame {
 
 	// Dodatkowe parametry okna
 		private String windowName = "Przelicznik";
+		
+	// Elementy okna
+		private JComboBox<String> list;
+			private int listX = 10;
+			private int listY = 10;
+			private int listWidth = 400;
+			private int listHeight = 20;
 	
 	public Window() {
-		setBounds(windowX, windowY, windowWidth, windowHeight);
-		setTitle(windowName);
-		setLayout(null);
+		// Ustawianie parametrów okna
+			setBounds(windowX, windowY, windowWidth, windowHeight);
+			setTitle(windowName);
+			setLayout(null);
+			
+		// Dodanie listy
+			list = new JComboBox<String>();
+			list.addItem("Tryb 1");
+			list.addItem("Tryb 2");
+			list.setBounds(listX, listY, listWidth, listHeight);
+			add(list);
+			
 		setVisible(true);
 	}
 }
