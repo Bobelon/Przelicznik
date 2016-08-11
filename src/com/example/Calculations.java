fb788e008c512170638238d105d24226160072c4
Filip <super.dewolaj@gmail.com>
2016-08-11 12:42:35 +0200

    Sprawdzanie poprawności danych w klasie "Obliczenia"

--- src/com/example/Calculations.java
+++ src/com/example/Calculations.java
@@ -3,10 +3,30 @@ package com.example;
 public class Calculations {
 	
 	public String dmsToDm(String degrees, String minutes, String seconds) {
-		return "DM = " + seconds;
+		
+		int D, M, S;
+		
+		try {
+			D = Integer.parseInt(degrees);
+			M = Integer.parseInt(minutes);
+			S = Integer.parseInt(seconds);
+		}
+		catch(NumberFormatException e) {
+			return "Niew³aœciwy format danych";
+		}
+		return "DM = " + D + " " + M + " " + S;
 	}
 	
-	public String dmToDms(String degrees, String minutes) {
-		return "DMS = " + degrees;
+	public String dmToDms(String degrees, String minutes) {		
+		int D, M;
+		
+		try {
+			D = Integer.parseInt(degrees);
+			M = Integer.parseInt(minutes);
+		}
+		catch(NumberFormatException e) {
+			return "Niew³aœciwy format danych";
+		}
+		return "DMS = " + D + " " + M;
 	}
 }
